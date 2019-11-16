b51f870e11e80af6b184c273b376e55fb749a856
YusrilAgisha12 <yusril.agisha12@gmail.com>
2019-11-16 13:50:47 +0700

    web gue

---
+++ precache-manifest.33352b88df7cae0f108a98c9227b0e85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408964e3d9a4bb93121d1918d23971ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b96062ecb6f9da05fa62",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7d53362b56dc68f7cf42",
+    "url": "/static/js/2.16ae547b.chunk.js"
+  },
+  {
+    "revision": "b96062ecb6f9da05fa62",
+    "url": "/static/js/main.4e65dcc5.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "c4fbdad50eee936242c4731218526e55",
+    "url": "/static/media/bg.c4fbdad5.png"
+  }
+]);
